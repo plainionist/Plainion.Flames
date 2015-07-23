b26f3b2a2e0cfb48c8a40ee5272be1ec81973a0e
ronin4net <stanley751@gmx.net>
2015-07-23 12:01:42 +0200

    minor code cleanup in PseudoMethods

--- src/Plainion.Flames/PseudoMethods.cs
+++ src/Plainion.Flames/PseudoMethods.cs
@@ -6,15 +6,18 @@ namespace Plainion.Flames.Model
     /// </summary>
     public static class PseudoMethods
     {
+        private const string Broken = "BROKEN";
+
         public static Method BrokenCallstackMethod(this TraceModelBuilder self)
         {
             // we need to route it through the builder in order to have it fully integrated (e.g. Symbols cache gets updated)
-            return self.CreateMethod("BROKEN", "BROKEN", "BROKEN", "BROKEN");
+            return self.CreateMethod(Broken, Broken, Broken, Broken);
         }
 
         public static bool IsBrokenCallstack(this Method self)
         {
-            return self.Module == "BROKEN" && self.Namespace == "BROKEN" && self.Class == "BROKEN" && self.Name == "BROKEN";
+            // ATTENTION: take care of backward compatibility here if u change the strings
+            return self.Module == Broken && self.Namespace == Broken && self.Class == Broken && self.Name == Broken;
         }
     }
 }
